6e231c457a0a0d576c66756e6714cba250daa653
geshtalt <lemantar.flcl@gmail.com>
2015-08-03 21:27:31 +0300

    choose from gallery implemented

--- app/src/main/java/com/gesthalt/myapplication/FilterActivity.java
+++ app/src/main/java/com/gesthalt/myapplication/FilterActivity.java
@@ -1,18 +1,92 @@
 package com.gesthalt.myapplication;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.loopj.android.http.RequestParams;
 
 
 public class FilterActivity extends ActionBarActivity {
+
+    private static int RESULT_LOAD_IMG = 1;
+    ProgressDialog prgDialog;
+    String encodedString;
+    RequestParams params = new RequestParams();
+    String imgPath, fileName;
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+        prgDialog = new ProgressDialog(this);
+        // Set Cancelable as False
+        prgDialog.setCancelable(false);
         
+    }
+
+    public void loadImageFromGallery(View view) {
+        // Create intent to Open Image applications like Gallery, Google Photos
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // Start the Intent
+
+        startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+    }
+
+
+    // When Image is selected from Gallery
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            // When an Image is picked
+            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
+                    && null != data) {
+                // Get the Image from data
+
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+                // Get the cursor
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                imgPath = cursor.getString(columnIndex);
+                cursor.close();
+                ImageView imgView = (ImageView) findViewById(R.id.imgView);
+                // Set the Image in ImageView
+                imgView.setImageBitmap(BitmapFactory
+                        .decodeFile(imgPath));
+                // Get the Image's file name
+                String fileNameSegments[] = imgPath.split("/");
+                fileName = fileNameSegments[fileNameSegments.length - 1];
+                // Put file name in Async Http Post Param which will used in Php web app
+                params.put("filename", fileName);
+
+            } else {
+                Toast.makeText(this, "You haven't picked Image",
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
+                    .show();
+        }
+
     }
 
     @Override
